f98105c02b0aa20f09f5eb8addd9e83c322af0c5
song suin <suinsong92@gmail.com>
2017-09-13 15:20:50 +0900

    2017-09-13-secret변경

---
+++ app/src/main/java/com/bit/naverbook/NaverSecret_sample.java
@@ -0,0 +1,13 @@
+package com.bit.naverbook;
+
+/**
+ * Created by suin on 2017-09-13.
+ */
+
+
+public class NaverSecret_sample {
+
+    public static final String NAVER_CLIENT_ID="naver_client_id";
+    public static final String NAVER_CLIENT_SECRET ="naver_client_secret";
+
+}
